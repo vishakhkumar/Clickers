e1b1d9ac5a13ba5204777eb4c339be8f72ae60da
Hackathon <hackathon@lawn-143-215-57-163.lawn.gatech.edu>
2016-09-24 08:36:24 -0400

    Starting again from nap. Just saving.

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+
+
+
---
+++ database.py
@@ -0,0 +1,17 @@
+
+from sql import *
+from sql.conditionals import *
+from sql.aggregate import *
+
+ClassName = 'Physics'
+
+user = Table(ClassName)
+select = user.select()
+
+
+
+
+
+
+
+
