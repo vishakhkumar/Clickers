c3766361bbaa0a6cd8d414550a116e0c79d3f79a
Hackathon <hackathon@lawn-143-215-57-163.lawn.gatech.edu>
2016-09-24 10:17:57 -0400

    Minor changes and testing.

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ from flask_json import FlaskJSON, JsonError, json_response, as_json
 #implementation of answer responses via JSON queries
 
 class answerresponse(object):
-
-  def __init__(self,StudentID=None,Answer=None):
-
+  def __init__(self,studentID=None,answer=None):
     # ask for forgiveness
-    try: self.studentID = 09090
+    try: self.studentID 
     except AttributeError:
       self.studentID = None
-    try: self.answer = 7878
+    try: self.answer
     except AttributeError:
       self.answer = None
-
     # assign values, even if they don't exist.
     self.studentID = studentID
     self.answer = answer
@@ -25,11 +22,10 @@ class answerresponse(object):
   def toJSON(self):
     return json_response(studentID = self.studentID,answer=self.answer)
 
-
   def fromJSON(self,info):
     processResponse = True
     try:
-        self.studentID = string(info['studentID'])
+        self.studentID = str(info['studentID'])
         self.answer = char(info['answer'])
     except (KeyError, TypeError, ValueError):
         processResponse = False
@@ -37,14 +33,29 @@ class answerresponse(object):
 
 app = Flask(__name__)
 
-@app.route('/answer')
+@app.route('/')
+def sup():
+  return "Sup, world!"
+
+@app.route('/test',methods=['GET'])
+def route():
+  print 'test'
+  return json_response(message="Hello!")
+
+@app.route('/answer',methods=['POST'])
 def answer():
+  print 'test'
   data = request.get_json(force=True)
-  studentanswer = answerresponse()
-  if studentanswer.fromJSON(data)=="Correct":
-    return json_response(message="Thank you")
-  else:
-    raise JsonError(description='Invalid value.')
+#  studentanswer = answerresponse()
+#  if studentanswer.fromJSON(data)=="Correct":
+#    return json_response(message="Thank you")
+#  else:
+#    raise JsonError(description='Invalid value.')
+#  print(data['studentID'])
+  print(console.log())
+  return json_response(message="Thank you")
+
+
 
 
 
