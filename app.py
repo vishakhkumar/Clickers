88d5ae97648c5862b35ad7d0055b2475940a0051
Hackathon <hackathon@lawn-143-215-57-163.lawn.gatech.edu>
2016-09-24 11:25:24 -0400

    Fuck commit messages. Made it future resistant with the library

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from __future__ import *
 from flask import Flask, request, jsonify
 #implementation of answer responses via JSON queries
 
@@ -13,19 +14,6 @@ class answerresponse(object):
     # assign values, even if they don't exist.
     self.studentID = studentID
     self.answer = answer
-
-
-  def toJSON(self):
-    return json_response(studentID = self.studentID,answer=self.answer)
-
-  def fromJSON(self,info):
-    processResponse = True
-    try:
-        self.studentID = str(info['studentID'])
-        self.answer = char(info['answer'])
-    except (KeyError, TypeError, ValueError):
-        processResponse = False
-    return processResponse
 
 app = Flask(__name__)
 
